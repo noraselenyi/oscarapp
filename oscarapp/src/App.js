b1e27adb17a52c15f55b81e78caeb38eb5addede
Nora Selenyi <macintosh@Nora-MacBook-Air.local>
2022-01-15 16:14:59 +0100

    changing landing page

--- oscarapp/src/App.js
+++ oscarapp/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the Oscar app
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.imdb.com/list/ls080103875/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Predictions for 2022
         </a>
       </header>
     </div>
