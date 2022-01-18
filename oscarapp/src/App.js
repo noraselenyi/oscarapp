697b000f3d82c93eb13b5270f1c0443471977397
eselnor <noraselenyi@gmail.com>
2022-01-18 13:57:56 +0100

    adding auth

--- oscarapp/src/App.js
+++ oscarapp/src/App.js
@@ -1,35 +1,45 @@
-import logo_oscars from './logo_oscars.jpg';
-import Filmlist from './components/filmlist';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+import SignIn from "./SignUp";
+import { AuthProvider } from "./Auth";
+import PrivateRoute from "./PrivateRoute";
+import logo_oscars from "./logo_oscars.jpg";
+import Filmlist from "./components/filmlist";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <img src={logo_oscars} className="oscarlogo" alt="logo" />
       </header>
-      <p>
-          Welcome to the Oscar app
-        </p>
-        <a
-          className="App-link"
-          href="https://www.imdb.com/list/ls080103875/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Predictions for 2022
-        </a>
-        <div className=''>
-        <p>  </p>
-        <p> LOGIN </p>
-        <p>  </p>
+
+      <AuthProvider>
+        <Router>
+          <div>
+            <PrivateRoute exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signin" component={SignIn} />
+          </div>
+        </Router>
+      </AuthProvider>
+      <p>Welcome to the Oscar app</p>
+      <a
+        className="App-link"
+        href="https://www.imdb.com/list/ls080103875/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Predictions for 2022
+      </a>
+      <div className="">
+        <p> </p>
         <Filmlist />
-        <p>here comes the list </p>
-          
-        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ oscarapp/src/base.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyD7944Q2inhm64lqOqT-K9Om-jIjhTBAxo",
+  authDomain: "filmapp-50de3.firebaseapp.com",
+  projectId: "filmapp-50de3",
+  storageBucket: "filmapp-50de3.appspot.com",
+  messagingSenderId: "361991253669",
+  appId: "1:361991253669:web:5c5857136ff0f73515e74c",
+});
+
+export default app;
+// 2:30nál átírni
